0888e44e6b2caa8c63580a01d6664596ce06f9df
GameChanger3714 <71977652+GameChanger3714@users.noreply.github.com>
2020-11-01 12:27:26 -0500

    Initial commit

---
+++ Phaser-Undertale-Battle/README.md
@@ -0,0 +1,7 @@
+# Undertale Battle
+
+## Install
+To install dependencies run 'npm install'
+
+## Run your server
+Build your code and start 
---
+++ Phaser-Undertale-Battle/index.js
@@ -0,0 +1,57 @@
+import Phaser from "phaser";
+
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update, 
+  },
+};
+
+var game = new Phaser.Game(config);
+this.heart
+
+function preload() {
+  this.load.image("heart", "https://64.media.tumblr.com/avatar_6a16f384117d_128.pnj");
+}
+
+function create() {
+  const heart = this.add.sprite(400, 300, "heart");
+  heart.displayHeight = 20;
+  heart.displayWidth = 20;
+  this.keys = this.input.keyboard.createCursorKeys();
+} 
+
+
+
+function update() {
+  this.heart = this.add.sprite(400, 300, "heart");
+  this.heart.displayHeight = 20;
+  this.heart.displayWidth = 20;
+
+  this.keys = this.input.keyboard.createCursorKeys();
+
+
+  this.heart
+  if (this.keys.up.isDown) {
+    this.heart.y -= 4;
+  }  
+  if (this.keys.down.isDown) {
+    this.heart.y += 4;
+  }
+  if (this.keys.right.isDown) {
+    this.heart.x += 4;
+  }
+  if (this.keys.left.isDown) {
+    this.heart.x -= 4;
+  }
+}
